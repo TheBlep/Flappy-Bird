a79a3f0a1cc30121b3d7f59ada1098fc50e792da
Timothy Randolph <Soshire@protonmail.com>
2025-01-27 11:43:39 +0000

    Restructing JS for easy game restart

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,115 +2,139 @@
  * All javascript is run once content has been loaded.
  */
 document.addEventListener('DOMContentLoaded', () => {
-    /**
-     * Setting Constants for the script to use
-     */
-    const bird = document.querySelector('.bird')
-    const gameDisplay = document.querySelector('.game-container')
-    const button = document.querySelector('.button')
-    /**const ground = document.querySelector('.ground')**/
-    
+    bird = document.querySelector('.bird')
+    gameDisplay = document.querySelector('.game-container')
+    button = document.querySelector('.button')
+    gameTimerId = setInterval(gameLoop, 20)
+    setUp()
 
-    let birdLeft = 220
-    let birdBottom = 100
-    let gravity = 2
-    let gap = 450
+    /** Button function to restart game */
+    button.onclick = function () {
+        console.log('isclicked')
+        setUp()
+        //gameTimerId = setInterval(startGame, 20),
+        //generateObstical(),
+        //isGameOver=false,
+        //setUp()
 
-    let isGameOver = false
+    };
+})
 
-    /**
-     * Function used to play the game
-     */
-    function startGame() {
-        birdBottom -= gravity
-        bird.style.bottom = birdBottom + 'px'
-        bird.style.left = birdLeft + 'px'
-        setInterval(gameTimerId, 20)
-        
-    }
+//Setting Constants for the script to use
+let birdLeft = 220
+let birdBottom = 100
+let gravity = 2
+let gap = 450
 
-    /**
-     * Let the game interval start every 20ms, used to simulate
-     * gravity using the birdBottom -= gravity
-     */
-    let gameTimerId = setInterval(startGame, 20)
+let isGameOver = false
 
-    /** Space bar jump trigger */
-    function control(e) {
-        if (e.keyCode === 32) {
-            jump()
-        }
-    }
-    /**document.getElementsByTagName("body").ontouchstart = jump;**/
+let bird = document.querySelector('.bird')
+let gameDisplay = document.querySelector('.game-container')
+let button = document.querySelector('.button')
 
-    /** Makes the bird jump */
-    function jump() {
-        // Creates a ceiling for the bird
-        if (birdBottom < 500)
-            // Adds 50px to bird bottom to use as a jump
-            birdBottom += 50
-        bird.style.bottom = birdBottom + 'px'
 
-    }
-    document.addEventListener('keyup', control)
-    document.addEventListener('touchstart', jump)
+/**Used to reset variables**/
+function setUp() {
 
-    /**Generates obsticals at random hights, moving at a constant pace left */
-    function generateObstical() {
-        let obsticalLeft = 500
-        // random hight in generation
-        let randomHeight = Math.random() * 60
-        let obsticalBottom = randomHeight
-        const obstical = document.createElement('div')
-        const topObstical = document.createElement('div')
-        if (!isGameOver) {
-            obstical.classList.add('obstical')
-            topObstical.classList.add('topObstical')
-        }
-        gameDisplay.appendChild(obstical)
-        gameDisplay.appendChild(topObstical)
-        obstical.style.left = obsticalLeft + 'px'
-        topObstical.style.left = obsticalLeft + 'px'
-        obstical.style.bottom = obsticalBottom + 'px'
-        topObstical.style.bottom = obsticalBottom + gap + 'px'
+    birdLeft = 220
+    birdBottom = 100
+    gravity = 2
+    gap = 450
 
-        /** moving obstical from right to left */
-        function moveObstical() {
-            obsticalLeft -= 2
-            obstical.style.left = obsticalLeft + 'px'
-            topObstical.style.left = obsticalLeft + 'px'
-
-            // remove obstical after leaving game area
-            if (obsticalLeft === -60) {
-                clearInterval(timerId)
-                gameDisplay.removeChild(obstical)
-                gameDisplay.removeChild(topObstical)
-            }
-            if (obsticalLeft > 200 && obsticalLeft < 270 &&
-                (birdBottom < obsticalBottom + 152 || birdBottom > obsticalBottom + gap - 200) ||
-                birdBottom === 0
-            ) {
-                gameOver()
-                clearInterval(timerId)
-
-            }
-        }
-        let timerId = setInterval(moveObstical, 20)
-        if (!isGameOver) setTimeout(generateObstical, 3000)
-        if (isGameOver) alert("You failed")
-    }
+    isGameOver = false
 
     generateObstical()
+    document.addEventListener('keyup', control)
+    document.addEventListener('touchstart', jump)
+}
 
-    /** end game function */
-    function gameOver() {
-        clearInterval(gameTimerId)
-        console.log('game over')
-        isGameOver = true
-        document.removeEventListener('keyup', control)
-        document.removeEventListener('touchstart', jump)
-    }
+/**Function used to play the game**/
+function gameLoop() {
     
-    /**button.onclick( gameTimerId = setInterval( startGame, 20));**/
-    /**button.addEventListener("click", gameTimerId = setInterval( startGame, 20));**/
-})
+    birdBottom -= gravity
+    bird.style.bottom = birdBottom + 'px'
+    bird.style.left = birdLeft + 'px'
+    
+}
+
+
+
+/**Let the game interval start every 20ms, used to simulate
+ * gravity using the birdBottom -= gravity
+ */
+
+
+/** Space bar jump trigger */
+function control(e) {
+    if (e.keyCode === 32) {
+        jump()
+    }
+}
+
+
+/** Makes the bird jump */
+function jump() {
+    // Creates a ceiling for the bird
+    if (birdBottom < 500)
+        // Adds 50px to bird bottom to use as a jump
+        birdBottom += 50
+    bird.style.bottom = birdBottom + 'px'
+
+}
+
+/**Generates obsticals at random hights, moving at a constant pace left */
+function generateObstical() {
+    let timerId = setInterval(moveObstical, 20)
+    let obsticalLeft = 500
+    // random hight in generation
+    let randomHeight = Math.random() * 60
+    let obsticalBottom = randomHeight
+    const obstical = document.createElement('div')
+    const topObstical = document.createElement('div')
+    if (!isGameOver) {
+        obstical.classList.add('obstical')
+        topObstical.classList.add('topObstical')
+    }
+    gameDisplay.appendChild(obstical)
+    gameDisplay.appendChild(topObstical)
+    obstical.style.left = obsticalLeft + 'px'
+    topObstical.style.left = obsticalLeft + 'px'
+    obstical.style.bottom = obsticalBottom + 'px'
+    topObstical.style.bottom = obsticalBottom + gap + 'px'
+
+    /** moving obstical from right to left */
+    function moveObstical() {
+        obsticalLeft -= 2
+        obstical.style.left = obsticalLeft + 'px'
+        topObstical.style.left = obsticalLeft + 'px'
+
+        // remove obstical after leaving game area
+        if (obsticalLeft === -60) {
+            clearInterval(timerId)
+            gameDisplay.removeChild(obstical)
+            gameDisplay.removeChild(topObstical)
+        }
+        if (obsticalLeft > 200 && obsticalLeft < 270 &&
+            (birdBottom < obsticalBottom + 152 || birdBottom > obsticalBottom + gap - 200) ||
+            birdBottom === 0
+        ) {
+            gameOver()
+            clearInterval(timerId)
+
+        }
+    }
+
+    if (!isGameOver) setTimeout(generateObstical, 3000)
+    if (isGameOver) alert("You failed")
+}
+
+/** end game function */
+function gameOver() {
+    clearInterval(gameTimerId)
+    console.log('game over')
+    isGameOver = true
+    document.removeEventListener('keyup', control)
+    document.removeEventListener('touchstart', jump)
+}
+
+//button.addEventListener("click", myScript);
